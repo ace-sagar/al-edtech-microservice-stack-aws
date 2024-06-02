9b10e9f0f11ba6897e54eae90645fec766df1193
Sagar Panwar <sagar.panwar@dreamorbit.com>
2024-06-02 21:48:24 +0530

    Initial commit

---
+++ test/al-edtech-microservice-stack-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AlEdtechMicroserviceStackAws from '../lib/al-edtech-microservice-stack-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/al-edtech-microservice-stack-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AlEdtechMicroserviceStackAws.AlEdtechMicroserviceStackAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
