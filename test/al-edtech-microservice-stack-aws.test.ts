8fff91410e9a08146e08cf44e0941cf69aeb8d0c
Sagar Panwar <sagarpanwar0123@gmail.com>
2024-06-14 22:23:23 +0530

    updated test cases for htttp verb

--- test/al-edtech-microservice-stack-aws.test.ts
+++ test/al-edtech-microservice-stack-aws.test.ts
@@ -3,8 +3,8 @@ import * as https from 'https';
 
 const apiGatewayUrl = 'https://o1cfqzl904.execute-api.ap-south-1.amazonaws.com/prod/';
 
-const httpsAgent = new https.Agent({  
-  rejectUnauthorized: false
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false
 });
 
 describe('AL World Infrastructure', () => {
@@ -15,20 +15,27 @@ describe('AL World Infrastructure', () => {
 
     it('should create a new employee (POST)', async () => {
         const employee = {
-            id: "1",
+            id: "10",
             name: "John Doe",
             role: "Developer"
         };
 
         const response = await axios.post(`${apiGatewayUrl}/employees`, employee, { httpsAgent });
         expect(response.status).toBe(201);
-        expect(response.data).toHaveProperty('id', '1');
+        expect(response.data).toEqual({
+            "message": "Employee created"
+        });
+        // expect(response.data).toHaveProperty('id', '1');
     });
 
     it('should retrieve an employee by ID (GET)', async () => {
-        const response = await axios.get(`${apiGatewayUrl}/employees/1`, { httpsAgent });
+        const response = await axios.get(`${apiGatewayUrl}/employees/10`, { httpsAgent });
         expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('name', 'John Doe');
+        expect(response.data).toEqual({
+            "id": "10",
+            "role": "Developer",
+            "name": "John Doe"
+        });
     });
 
     it('should update an employee by ID (PATCH)', async () => {
@@ -37,15 +44,19 @@ describe('AL World Infrastructure', () => {
             role: "Senior Developer"
         };
 
-        const response = await axios.patch(`${apiGatewayUrl}/employees/1`, updateData, { httpsAgent });
+        const response = await axios.patch(`${apiGatewayUrl}/employees/10`, updateData, { httpsAgent });
         expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('name', 'John Doe Updated');
+        expect(response.data).toEqual({
+            "message": "Employee updated"
+        });
     });
 
     it('should delete an employee by ID (DELETE)', async () => {
-        const response = await axios.delete(`${apiGatewayUrl}/employees/1`, { httpsAgent });
+        const response = await axios.delete(`${apiGatewayUrl}/employees/10`, { httpsAgent });
         expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('message', 'Employee deleted');
+        expect(response.data).toEqual({
+            "message": "Employee deleted"
+        });
     });
 
 });
